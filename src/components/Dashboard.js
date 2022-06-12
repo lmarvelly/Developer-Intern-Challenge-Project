5ed410c0ff4cadddae33321bc9ce8768d3c8cf8b
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-12 18:36:10 +0100

    Adding headers, horizontal lines and spacing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,9 +5,19 @@ import InventoryList from './InventoryList';
 
 const Dashboard = () => (
 	<div className='content-container'>
+		<h1>Add New Item</h1>
 		<InventoryForm />
 		<hr />
+
+		<h1>Add New Werehouse</h1>
+		<span>TODO: Add werehouse form here</span>
+		<hr />
+
 		<InventoryList />
+
+		<h1>Werehouse List</h1>
+		<span>TODO: Add werehouse list</span>
+
 	</div>
 );
 
