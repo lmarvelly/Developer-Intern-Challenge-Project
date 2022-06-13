0453bb5ff4853fb9094f2cbbab8768c46506e509
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-13 12:41:19 +0100

    Adding empty array in case of error in retrieving data

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -11,7 +11,7 @@ class InventoryList extends Component
 
 		this.state = 
 		{
-			inventoryList: getDatabase().inventory
+			inventoryList: getDatabase().inventory ? getDatabase().inventory : [] 
 		}
 	}
 
