732f2085bb94a572dff614ab62b59d2aaf1f75a1
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-14 16:28:24 +0100

    Adding dispatches to retrieve inventory and warehouses

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ import './styles/styles.scss'; // SASS styles form
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import Dashboard from './components/Dashboard';
+import { setInventory } from './actions/inventory';
+import { setWarehouses } from './actions/warehouses';
+import { getDatabase } from './actions/databaseFunctions';
 
 const store = configureStore();
 
@@ -34,30 +37,6 @@ const store = configureStore();
 // Render Loading Message
 ReactDOM.render(jsx, document.getElementById('app'));
 
-// Firebase authentication
-// firebase.auth().onAuthStateChanged((user) =>
-// {
-// 	if(user)
-// 	{
-// 		store.dispatch(login(user.uid));
-// 		// App renders after Promises are complete
-// 		store.dispatch(startSetMembers());
-// 		store.dispatch(startSetSeasons());
-// 		store.dispatch(startSetRecords())
-// 			.then(() =>
-// 			{
-// 				store.dispatch( sortAlphabetAsc() ); // Should sort Member alphabetically
-// 				renderApp();
-// 				if(history.location.pathname === '/')
-// 				{
-// 					history.push('/dashboard');
-// 				};
-// 			});
-// 	}
-// 	else
-// 	{
-// 		store.dispatch(logout())
-// 		renderApp();
-// 		history.push('/');
-// 	}
-// });
+// TODO: dispatches
+store.dispatch(setWarehouses(getDatabase().warehouses));
+store.dispatch(setInventory(getDatabase().inventory));
