d0eca930aff036eeba8c5890a92f16886c5b0cff
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-14 19:06:44 +0100

    Removing import

--- src/components/WarehouseList.js
+++ src/components/WarehouseList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getDatabase } from '../actions/databaseFunctions';
 import WarehouseItem from './WarehouseItem';
 
 class WarehouseList extends Component
