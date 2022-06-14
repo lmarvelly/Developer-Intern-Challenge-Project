c3a216a82becb5ad508b593edefa1d8d7499d0b2
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-14 10:45:19 +0100

    Configuring firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,32 +1,23 @@
-// New Syntax:
 // Gets all the variables in 'firebase' and dumps them into the variable firebase
 import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = 
 {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID
+	apiKey: "AIzaSyAiFt3tmiQUCJOGnTr8V0Bd3CIEXo9v99I",
+	authDomain: "shopify-challenge-project.firebaseapp.com",
+	databaseURL: "https://shopify-challenge-project-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "shopify-challenge-project",
+	storageBucket: "shopify-challenge-project.appspot.com",
+	messagingSenderId: "497925478052",
+	appId: "1:497925478052:web:07f9e504ae6c4ee337f831",
+	measurementId: "G-MFGGN6YNPE"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-const auth = firebase.auth();
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-
-// Force Firebase to select a Google account everytime user logs in
-googleAuthProvider.setCustomParameters(
-{
-	'prompt': 'select_account'
-});
-
-export { auth, firebase, googleAuthProvider, database as default };
+export { firebase, database as default };
